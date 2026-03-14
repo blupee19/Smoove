a47c64c8f25d3a0e6e18f0aef3c2fbe84efb20f0
mayank <mayank290404gupta@gmail.com>
2026-03-14 17:36:56 +0530

    first commit for this branch

--- Assets/Scripts/Script.cs
+++ Assets/Scripts/Script.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Script : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Start() // hello
     {
         
     }
